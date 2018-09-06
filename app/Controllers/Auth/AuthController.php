6ea4eaca26715cc52ad67157fdccf350c5b3aa96
joel123vilca <joevil23tjq@gmail.com>
2018-09-05 21:57:39 -0500

    signing the user up

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -1,6 +1,6 @@
 <?php
   namespace App\Controllers\Auth;
-
+  use App\Models\User;
   use App\Controllers\Controller;
 
 
@@ -10,9 +10,14 @@
       {
         return $this->view->render($response, 'auth/signup.twig');
       }
-      public function PostSignUp()
+      public function PostSignUp($request, $response)
       {
-
+        User::create([
+          'email'=>$request->getParam('email'),
+          'name'=> $request->getParam('name'),
+          'password'=>password_hash($request->getParam('password'),PASSWORD_DEFAULT),
+        ]);
+        return $response->withRedirect($this->router->pathFor('home'));
       }
   }
 
--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,7 @@
 //   return $this->view->render($response, 'home.twig');
 // });
 
-$app->get('/','HomeController:index');
+$app->get('/','HomeController:index')->setName('home');
 
 $app->get('/auth/signup','AuthController:getSignUp')->setName('auth.signup');
 $app->post('/auth/signup','AuthController:postSignUp');
